e8c54d412742f0c96b613b130a0396b8c3c35259
Alexandr Drozdov <drozd-aleks@bk.ru>
2020-09-30 13:45:50 +0300

    1.3 commit variables

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,13 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let credit = amount - contribution;
+    let monthlyPercent = percent / 100 / 12;
+    let month = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
+    let monthlyPayment = credit * (monthlyPercent + monthlyPercent / (((1 + monthlyPercent) ** month) - 1));
+    let totalAmount = month * monthlyPayment;
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    greeting = (name == null || name.length === 0 ? `Привет, мир! Меня зовут Аноним` : `Привет, мир! Меня зовут ${name}`);
+    return greeting;
 }
